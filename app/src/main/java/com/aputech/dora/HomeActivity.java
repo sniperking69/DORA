8f946a64a96990a799deecc0dc04dd420193f76e
Adan Nelson <adan.nelson28@gmail.com>
2019-10-14 23:52:28 +0500

    launch activity fixed, added icons for HomeActivity & toolbar

--- app/src/main/java/com/aputech/dora/HomeActivity.java
+++ app/src/main/java/com/aputech/dora/HomeActivity.java
@@ -5,17 +5,19 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar= findViewById(R.id.toolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -23,7 +25,7 @@ public class HomeActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_trending, R.id.navigation_new,R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(toolbar, navController,appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
