3ee4dc7fde947d23ab4d0c1293971451d7f5b6fc
Adan Nelson <adan.nelson28@gmail.com>
2019-12-25 20:40:14 +0500

    up

--- app/src/main/java/com/aputech/dora/ui/MainActivity.java
+++ app/src/main/java/com/aputech/dora/ui/MainActivity.java
@@ -1,31 +1,70 @@
 package com.aputech.dora.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.aputech.dora.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final int MY_REQUEST_CODE = 457;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this,HomeActivity.class));
-                finish();
-            }
-        },5000);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                    finish();
+                }
+            },1500);
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setTheme(R.style.Nobar)
+                            .setLogo(R.drawable.ic_logo)
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.PhoneBuilder().build()))
+                            .build(),
+                    MY_REQUEST_CODE);
+        }
+
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(this, HomeActivity.class));
+                finish();
+                Toast.makeText(this, "Welcome! " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "" + response.getError().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
 
