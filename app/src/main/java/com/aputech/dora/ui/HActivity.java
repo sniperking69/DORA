ede56962c64fa5b3ab441edf220c5826fc5a16ca
Adan Nelson <adan.nelson28@gmail.com>
2020-02-13 15:20:55 +0500

    Logout test button added

--- app/src/main/java/com/aputech/dora/ui/HActivity.java
+++ app/src/main/java/com/aputech/dora/ui/HActivity.java
@@ -6,10 +6,14 @@ import android.view.Menu;
 import android.view.View;
 
 import com.aputech.dora.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -36,8 +40,13 @@ public class HActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
-                startActivity(intent);
+                AuthUI.getInstance().signOut(HActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // user is now signed out
+                                startActivity(new Intent(HActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        });
             }
         });
     }
