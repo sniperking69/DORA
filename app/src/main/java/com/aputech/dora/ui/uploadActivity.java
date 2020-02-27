3db921388a009c909d74097d58637d6d5d764ffe
Adan Nelson <adan.nelson28@gmail.com>
2020-02-27 18:17:50 +0500

    added upload activity

---
+++ app/src/main/java/com/aputech/dora/ui/uploadActivity.java
@@ -0,0 +1,23 @@
+package com.aputech.dora.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.aputech.dora.R;
+
+public class uploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+
+    public void uploading_Data(View view) {
+    }
+
+    public void setLocation(View view) {
+    }
+}
