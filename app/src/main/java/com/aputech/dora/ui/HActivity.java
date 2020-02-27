9dce2a14926d34f8b59100bd0e934a84fbf7ebc1
Ahsan <mahsanjoiya@gmail.com>
2020-02-27 18:55:01 +0500

    Changes made to Home activity

--- app/src/main/java/com/aputech/dora/ui/HActivity.java
+++ app/src/main/java/com/aputech/dora/ui/HActivity.java
@@ -3,6 +3,7 @@ package com.aputech.dora.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.aputech.dora.R;
@@ -15,26 +16,51 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class HActivity extends AppCompatActivity {
-
+    public BottomNavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_h);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
+        BottomNavigationView.OnNavigationItemSelectedListener listener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent intent = new Intent(HActivity.this,uploadActivity.class);
+                        startActivity(intent);
+                        return true;
+//                    case R.id.:
+//                        toolbar.setTitle("My Gifts");
+//                        return true;
+//                    case R.id.navigation_cart:
+//                        toolbar.setTitle("Cart");
+//                        return true;
+//                    case R.id.navigation_profile:
+//                        toolbar.setTitle("Profile");
+//                        return true;
+                }
+                return false;
+            }
+        };
+        navView.setOnNavigationItemSelectedListener(listener);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-      NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//      NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +76,14 @@ public class HActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+    @Override
+    protected void onStart() {
+        navView.setSelectedItemId(R.id.trending);
+        super.onStart();
+
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
